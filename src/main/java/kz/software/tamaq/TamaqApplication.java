f031ac5577fbd7ed730d614fdecc037b4b76d8ef
Anarbayev Yeldos <eldosanarbaev7@gmail.com>
2023-06-16 19:21:51 +0900

    "First commit"

---
+++ src/main/java/kz/software/tamaq/TamaqApplication.java
@@ -0,0 +1,13 @@
+package kz.software.tamaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/software/tamaq/TamaqApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.software.tamaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
